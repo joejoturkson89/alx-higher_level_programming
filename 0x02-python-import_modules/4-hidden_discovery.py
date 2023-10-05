a417fddf871d90454dda2e0961c6c57a2f8cea78
joejoturkson89 <turksonjoejo@gmail.com>
2023-10-05 09:40:13 -0700

    4 hidden discovery

---
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+if __name__ == "__main__":
+    import hidden_4 as hidden
+    list = dir(hidden)
+    for i in range(len(list)):
+        if(list[i][0] != '_')
+        print(list[i])
