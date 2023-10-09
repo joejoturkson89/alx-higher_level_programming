a94fb64f6532db6108f15391607d6c49a0bfbbe0
joejoturkson89 <turksonjoejo@gmail.com>
2023-10-09 09:12:44 -0700

    13 list is a palindrome

---
+++ 0x03-python-data_structures/13-is_palindrome.c
@@ -0,0 +1,38 @@
+#include "lists.h"
+/**
+ * is_palindrome - function that checks if a linked list is a palindrome
+ * @head: pointer to the head of the linked list
+ * Return: return 0 if its not a palindrome or 1 if its a palindrome
+ * An empty list is considered a palindrome
+ */
+int is_palindrome(listint_t **head)
+{
+	listint_t *fast = *head;
+	listint_t *slow = *head;
+	listint_t *prev = NULL;
+	listint_t *temp;
+
+	if (*head == NULL || (*head)->next == NULL)
+		return (1);
+
+	while (fast != NULL && fast->next != NULL)
+	{
+		fast = fast->next->next;
+		temp = slow->next;
+		slow->next = prev;
+		prev = slow;
+		slow = temp;
+	}
+	if (fast != NULL)
+		slow = slow->next;
+
+	while (prev != NULL && slow != NULL)
+	{
+		if (prev->n != slow->n)
+			return (0);
+		prev = prev->next;
+		slow = slow->next;
+	}
+
+	return (1);
+}
