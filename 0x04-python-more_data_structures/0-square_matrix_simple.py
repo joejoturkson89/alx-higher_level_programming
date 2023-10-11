ca5755bd580ea2e2b3759c0c3a5fcdfabfee2338
Joejo Turkson <turksonjoejo@gmail.com>
2023-10-11 13:09:16 +0000

    0 square matrix simple

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -1,4 +1,8 @@
 #!/usr/bin/python3
 def square_matrix_simple(matrix=[]):
-    if(matrix):
-	return ([[y*y for y in x] for x in matrix])
+	new_list = []
+	if len(matrix) == 0:
+		return new_list
+
+	new_list = [[i*i for i in j] for j in matrix]
+	return new_list
