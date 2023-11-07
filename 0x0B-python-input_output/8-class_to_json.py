56546830653f6b4cb67822e240d9c172d30c4d3b
joejoturkson89 <turksonjoejo@gmail.com>
2023-11-07 09:34:18 -0800

    8 claas to JSON

---
+++ 0x0B-python-input_output/8-class_to_json.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+"""Function defines Python class-to-JSON."""
+
+
+def class_to_json(obj):
+    """This returns the dictionary representation of a simple data structure."""
+    return obj.__dict__
