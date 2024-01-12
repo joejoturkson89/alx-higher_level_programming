103121cbfb0ea1b02eabe7c465e7d5046f321004
joejoturkson89 <turksonjoejo@gmail.com>
2024-01-12 05:45:22 -0800

    0 select states

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -2,22 +2,11 @@
 """
 Lists all states from the database hbtn_0e_0_usa.
 """
-
-def main(args):
-
-    if len(args) != 4:
-        raise Exception("need 3 arguments!")
-    db = MySQLdb.connect(host='localhost',
-            user=args[1],
-            passwd=args[2],
-            db=args[3])
-    c = db.cursor()
-    c.execute("SELECT * FROM states ORDER BY id ASC")
-    states = c.fecthall()
-    for state in states:
-        print(state)
+import sys
+import MySQLdb
 
 if __name__ == "__main__":
-    import sys
-    import MySQLdb
-    main(sys.argv)
+    db = MySQLdb.connect(user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3])
+    cur = db.cursor()
+    cur.execute("SELECT * FROM 'states'")
+    [print(state) for state in cur.fetchall()]
