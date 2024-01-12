f56a9f116417d6e1fe3d7ae829d299ea51bbdb63
joejoturkson89 <turksonjoejo@gmail.com>
2024-01-12 06:22:32 -0800

    3 my safe filter

--- 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
+++ 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -2,8 +2,8 @@
 """
 Lists all states from the database hbtn_0e_0_usa.
 """
-import sys
 import MySQLdb
+from sys import argv
 
 if __name__ == "__main__":
     db = MySQLdb.connect(host="localhost", port=3306, user=argv[1],
