8ffb7cc011a7cbdec447d708afdc293d978c7928
joejoturkson89 <turksonjoejo@gmail.com>
2024-01-26 06:00:43 -0800

    10 my github-8 json api

---
+++ 0x11-python-network_1/10-my_github.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""This script takes your GitHub credentials
+(username and password) and uses the GitHub API to display your id."""
+
+import sys
+import requests
+import requests.auth
+import HTTPBasicAuth
+
+if __name__ == "__main__":
+    auth = HTTPBasicAuth(sys.argv[1], sys.argv[2])
+    r = requests.get("http//api.github.com/user", auth=auth)
+    print(r.json().get("id"))
