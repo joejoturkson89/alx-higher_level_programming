d99b0a322d8ba9b3bb3ae0af030930dc0a39f315
joejoturkson89 <turksonjoejo@gmail.com>
2024-02-20 04:54:13 -0800

    star wars title

--- 0x14-javascript-web_scraping/3-starwars_title.js
+++ 0x14-javascript-web_scraping/3-starwars_title.js
@@ -5,10 +5,7 @@ const num = process.argv[2];
 request('http://swapi.co/api/films/' + num, function (error, response, body) {
   if (error) {
     console.log(error);
-  } else if (response.statusCode === 200) {
-    const responseJSON = JSON.parse(body);
-    console.log(responseJSON.title);
   } else {
-    console.log('Error code: ' + response.statusCode);
+    console.log(JSON.parse(body)["title"]);
   }
 });
